e19f0d03690e9059674f4947339f36c5764a9479
Andhika Satrya <andhikangurah@gmail.com>
2024-07-29 18:42:15 +0800

    Add the solution to the problem [Twice linear]

---
+++ Javascript/Twice linear/dblLinear.js
@@ -0,0 +1,35 @@
+function dblLinear(n) {
+    const LIMIT = 1000000;
+    const sequence = new Array(LIMIT).fill(0); // Create container for the sequence
+
+    let doublePlusOneIndex = 0, triplePlusOneIndex = 0; // Index that points to the last numbers generated, both start at 0
+    let doublePlusOne = 1, triplePlusOne = 1; // Latest numbers generated
+
+    for (let i = 0; i < LIMIT; i++ ) // Generate the sequence
+    {
+        /* Determine the minimum,
+        if the latest 2n + 1 and 3n + 1 are the same, generate both,
+        otherwise generate only the smaller value.
+        This will ensure that they are sorted without the need to sort again */
+        const minimum = (doublePlusOne <= triplePlusOne) ? doublePlusOne : triplePlusOne;
+        if (doublePlusOne == minimum)
+        {
+        sequence[i] = doublePlusOne; // Save the value at the back of array
+        doublePlusOne = sequence[doublePlusOneIndex] * 2 + 1; // Generate the next 2n + 1 number
+        doublePlusOneIndex++; // Move the index to the next number to generate more 2n + 1 numbers
+        }
+        if (triplePlusOne == minimum)
+        {
+        sequence[i] = triplePlusOne; // Save the value at the back of array
+        triplePlusOne = sequence[triplePlusOneIndex] * 3 + 1; // Generate the next 3n + 1 number
+        triplePlusOneIndex++; // Move the index to the next number to generate more 3n + 1 numbers
+        }
+    }
+
+    return sequence[n]; // Once generated, just return the n-th value
+}
+
+console.log(dblLinear(10), 22);
+console.log(dblLinear(20), 57);
+console.log(dblLinear(30), 91);
+console.log(dblLinear(50), 175);
